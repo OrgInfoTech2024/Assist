5e93c66c96ce11df735b3175a05a16d1a1292818
OrgInfoTech <orginfotech@ukr.net>
2024-07-23 09:10:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+import cohere
+import pyttsx3
 import sys
 
 class Assist(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.initUI()
+        self.initAPI()
+        self.initTTS()
+
+    def initUI(self):
         
         self.setWindowTitle("Assist")
         self.setWindowIcon(QIcon("images/icon.png"))
@@ -20,9 +28,12 @@ class Assist(QMainWindow):
         layout.addLayout(bottomLayout, 1, 0)
         bottomLayout.addLayout(buttonsLayout, 0, 1)
 
-        self.chatListbox = QListView(self)
+        self.chatListbox = QListWidget(self)
         layout.addWidget(self.chatListbox, 0, 0, 1, 2)
         
+        self.chatListbox.setContextMenuPolicy(Qt.CustomContextMenu)
+        self.chatListbox.customContextMenuRequested.connect(self.showContextMenu)
+                
         self.input = QLineEdit(self)
         self.input.setPlaceholderText("Write your query")
         bottomLayout.addWidget(self.input, 0, 0, 1, 1)
@@ -38,11 +49,58 @@ class Assist(QMainWindow):
         buttonsLayout.addWidget(self.sendButton)
         self.sendButton.clicked.connect(self.send)
         
+    def initAPI(self):
+        self.api_key = 'DL3ttePi3LvY2QuUTgWNfURVeKgh762KRjXZ3ACh'
+        self.client = cohere.Client(self.api_key)
+        
+    def initTTS(self):
+        self.engine = pyttsx3.init()
+        
     def send(self):
-        return
+        userText = self.input.text()
+        self.chatListbox.addItem(f"You:\n\n{str(userText)}")
+        
+        try:
+            response = self.client.generate(
+                model='command-xlarge-nightly',
+                prompt=userText,
+                max_tokens=1000
+            )
+            assistText = response.generations[0].text.strip()
+            self.chatListbox.addItem(f"Assist:\n\n{assistText}")
+        except Exception as e:
+            self.chatListbox.addItem(f"Assist:\n\nError: {str(e)}")
+        
+        self.input.clear()
         
     def voice(self):
         return
+    
+    def showContextMenu(self, position):
+        contextMenu = QMenu(self)
+        copyAction = QAction("Copy", self)
+        listAction = QAction("List", self)
+        
+        contextMenu.addAction(copyAction)
+        contextMenu.addAction(listAction)
+        
+        copyAction.triggered.connect(self.copyItem)
+        listAction.triggered.connect(self.sayItem)
+        
+        contextMenu.exec_(self.chatListbox.viewport().mapToGlobal(position))
+        
+    def copyItem(self):
+        selectedItems = self.chatListbox.selectedItems()
+        if selectedItems:
+            clipboard = QApplication.clipboard()
+            clipboard.setText(selectedItems[0].text())
+    
+    def sayItem(self):
+        selectedItems = self.chatListbox.selectedItems()
+        if selectedItems:
+            text = selectedItems[0].text()
+            self.engine.say(text)
+            self.engine.runAndWait()
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
